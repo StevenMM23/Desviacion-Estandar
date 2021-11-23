98b82c658972f90bb81e6e86362278f4603c6e50
Steven Martinez <esteven123123@outlook.com>
2021-11-22 21:48:01 -0400

    Add Prototypes and Main

--- Desviacion-Estandar/Desviacion-Estandar.cpp
+++ Desviacion-Estandar/Desviacion-Estandar.cpp
@@ -1,8 +1,27 @@
+#include <cmath>
 #include <iostream>
 using namespace std;
 
+float StandardDesv(float cal[]);
+float HigherCal(float cal[]);
+
 int main()
 {
-   cout << "Hello World!\n";
+	float cal[10];
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Enter the " << i + 1 << " number:";
+		cin >> cal[i];
+	}
+	cout << "Higher Calification = " << HigherCal(cal);
+	cout << "Standard Deviation = " << StandardDesv(cal);
 }
+
+
+
+
+
+
+
+
 
